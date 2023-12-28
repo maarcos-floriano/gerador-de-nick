02c9fafea95508ed1d08823a9fee812422db2559
maarcos01 <toretomarcos50@gmail.com>
2023-12-28 19:11:21 -0300

    criei a funcao de gerar nicks e criar email

---
+++ public/main.js
@@ -0,0 +1,57 @@
+function gerarNick(nome) {
+  const titulos = [
+    "Bravo",
+    "Destemido",
+    "Mestre",
+    "Rápido",
+    "Sábio",
+    "Audaz",
+    "Silencioso",
+    "Misterioso",
+    "Poderoso",
+    "Lendário",
+    "Astuto",
+    "Invencível",
+    "Ágil",
+    "Supremo",
+    "Furtivo",
+    "Brilhante",
+    "Infalível",
+    "Valente",
+    "Majestoso",
+    "Implacável",
+    "Relâmpago",
+    "Intrépido",
+    "Magnífico",
+    "Incansável",
+    "Imortal",
+    "Glorioso",
+    "Feroz",
+    "Incrível",
+    "Audacioso",
+  ];
+  nicksEscolhidos = [];
+  for (let i = 0; i <= 4; i++) {
+    let tituloEscolhido = titulos[Math.floor(Math.random() * titulos.length)];
+    nicksEscolhidos.push(tituloEscolhido + nome.toUpperCase());
+  }
+
+  return nicksEscolhidos;
+}
+function criarEmail(nick) {
+  dominios = ["@gmail.com", "@hotmail.com", "@outlook.com", "@yahoo.com"];
+  let emailComNick = [];
+  for (let i = 0; i < dominios.length; i++) {
+
+    emailComNick.push(nick[i] + dominios[i]);
+  }
+
+  return emailComNick;
+}
+
+function criarPerfil() {
+  let nome = document.getElementById("ipt_nome").value;
+  nickName = gerarNick(nome);
+  email = criarEmail(nickName);
+
+}
